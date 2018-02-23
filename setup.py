3c2eddc77de0043af5fe89db3cad4f62e9002c09
Nick McCoy <nick@stitchdata.com>
2018-02-23 13:21:09 -0500

    bumping version to 0.0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tap-exacttarget',
-    version='0.0.7',
+    version='0.0.8',
     description='Singer.io tap for extracting data from the ExactTarget API',
     author='Fishtown Analytics',
     url='http://fishtownanalytics.com',
