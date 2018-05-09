41719e3e8ac9f1f1c21109a5481afc38f7418d3c
Kyle Allan <kyle@stitchdata.com>
2018-05-09 16:39:58 -0400

    bump version

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Changelog
+
+## 1.0.4
+  * Skip empty subscriber batches [#9](https://github.com/singer-io/tap-exacttarget/pull/9)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tap-exacttarget',
-    version='1.0.3',
+    version='1.0.4',
     description='Singer.io tap for extracting data from the ExactTarget API',
     author='Fishtown Analytics',
     url='http://fishtownanalytics.com',
