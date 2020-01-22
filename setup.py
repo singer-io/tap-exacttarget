95205c865f61262c47499547c0b48ee456082424
Kyle Allan <kyle@stitchdata.com>
2020-01-22 15:42:18 +0000

    set singer-python as a specific version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     py_modules=['tap_exacttarget'],
     install_requires=[
         'funcy==1.9.1',
-        'singer-python>=5.3.1',
+        'singer-python==5.9.0',
         'python-dateutil==2.6.0',
         'voluptuous==0.10.5',
         'Salesforce-FuelSDK==1.3.0'
