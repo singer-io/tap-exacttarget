f8a29069f8b6f6e86bb938db5425291fe8570fb2
harshpatel4_crest <harsh.patel4@crestdatasys.com>
2021-09-01 20:22:26 +0530

    resolve tes case failure

--- tests/test_exacttarget_bookmarks.py
+++ tests/test_exacttarget_bookmarks.py
@@ -38,6 +38,7 @@ class ExactTargetBookmarks(ExactTargetBase):
         replication_keys = self.expected_replication_keys()
         for stream in expected_streams:
             if self.is_incremental(stream):
+                new_state['bookmarks'][stream] = dict()
                 new_state['bookmarks'][stream]['field'] = next(iter(replication_keys[stream]))
                 new_state['bookmarks'][stream]['last_record'] = '2021-01-01T00:00:00Z'
 
