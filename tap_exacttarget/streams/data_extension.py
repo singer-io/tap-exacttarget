d82a037546cec334d0836bce190a389688d99d1d
vishalpachpinde <vpachpinde@talend.com>
2025-11-13 17:38:53 +0530

    remove platformnobj

--- tap_exacttarget/streams/data_extension.py
+++ tap_exacttarget/streams/data_extension.py
@@ -12,4 +12,11 @@ class DataExtension(FullTableStream):
     tap_stream_id = "data_extension"
     object_ref = "DataExtension"
     key_properties = ["ID"]
+
     # repl_keys = ["ModifiedDate"]
+    def get_query_fields(self, *args, **kwargs):
+        """Filter Query fields."""
+        q_fields = self.get_available_fields()
+        if "IsPlatformObject" in q_fields:
+            q_fields.remove("IsPlatformObject")
+        return q_fields
