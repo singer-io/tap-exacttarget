1a2239b35428fb64716839e1b4e1acbe6b872555
vishalpachpinde <vpachpinde@talend.com>
2025-11-18 00:08:42 +0530

    fix pylint

--- tap_exacttarget/streams/dataextensionobjects.py
+++ tap_exacttarget/streams/dataextensionobjects.py
@@ -18,6 +18,7 @@ class DataExtensionObjectBase:
     schema = None
     customer_key = None
     category_id = None
+    object_ref = None
 
     def get_query_fields(self, stream_metadata: Dict, schema: Dict):
         """Filter fields to query from metadata."""
