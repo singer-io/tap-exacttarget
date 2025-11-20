3ed389f1f05001a243f6cce33c439523218bde5e
vishalpachpinde <vpachpinde@talend.com>
2025-11-20 16:43:20 +0530

    added comments

--- tap_exacttarget/streams/automation.py
+++ tap_exacttarget/streams/automation.py
@@ -4,13 +4,24 @@ from tap_exacttarget.streams.abstracts import FullTableStream
 
 
 class Automation(FullTableStream):
-    """Class for List Send stream."""
+    """Class for Automation stream."""
+
+        # INFO Objtype: Automation fields: ['ObjectID', 'Name', 'Description', 'Schedule.ID', 'CustomerKey', 'Client.ID', 'IsActive', 'CreatedDate',
+    # 'Client.CreatedBy', 'ModifiedDate', 'Client.ModifiedBy', 'Status', 'Client.EnterpriseID']
+
+    # ID not retrievable
+    # ObjectID is retrievable but also null
+    # Single Record with almost all null values
+    # Null values for replication key(expected), unable to test pagination etc "ModifiedDate": null,
+    # no records with any valiid replication key value
+
 
     client: Client
 
     stream = "automation"
     tap_stream_id = "automation"
     object_ref = "Automation"
-    key_properties = ["ID"]
+    key_properties = ["ObjectID"]
     replication_key = "ModifiedDate"
     valid_replication_keys = ["ModifiedDate"]
+
--- tap_exacttarget/streams/send_summary.py
+++ tap_exacttarget/streams/send_summary.py
@@ -6,6 +6,16 @@ from tap_exacttarget.streams.abstracts import FullTableStream
 class SendSummary(FullTableStream):
     """Class for SendSummary stream."""
 
+    # https://developer.salesforce.com/docs/marketing/marketing-cloud/guide/sendsummary.html
+
+    # Error
+    # singer.transform.SchemaMismatch: Errors during transform
+    #     ID: data does not match {'type': 'integer'}
+
+    # Supported Fields
+    # INFO Objtype: SendSummary fields: ['Client.ID', 'AccountID', 'SendID', 'DeliveredTime', 'CreatedDate', 'ModifiedDate', 'CustomerKey', 'PartnerKey', 'AccountName', 'AccountEmail', 'IsTestAccount', 'TotalSent', 'Transactional', 'NonTransactional']
+    # ID or ObjectID missing in supported fields, might be related to a permission issue
+
     client: Client
 
     stream = "send_summary"
