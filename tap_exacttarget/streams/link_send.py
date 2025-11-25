6b1201ce5f6c7706b05a1dda14682a0faef4cb47
vishalpachpinde <vpachpinde@talend.com>
2025-11-25 16:49:03 +0530

    added link send

--- tap_exacttarget/streams/link_send.py
+++ tap_exacttarget/streams/link_send.py
@@ -1,15 +1,11 @@
 from typing import Dict
 from tap_exacttarget.client import Client
-from tap_exacttarget.streams.abstracts import IncrementalStream
+from tap_exacttarget.streams.abstracts import FullTableStream
 
 
-class LinkSend(IncrementalStream):
+class LinkSend(FullTableStream):
     """Class for Link Send stream."""
     # https://developer.salesforce.com/docs/marketing/marketing-cloud/guide/linksend.html
-
-    # Error:
-    # singer.transform.SchemaMismatch: Errors during transform
-    #     IDLong: data does not match {'type': 'integer'} -- Changed to ID and added null
 
     # Works Well as full table but not incremental
     # Supported Fields
@@ -18,11 +14,13 @@ class LinkSend(IncrementalStream):
     #     raise MarketingCloudError(response["OverallStatus"])
     # tap_exacttarget.exceptions.MarketingCloudError: Error: The Filter Property 'ModifiedDate' is not a retrievable property.
 
+    # Can be implemented as psuedo incremental but no valid records available with a ModifiedDate thus making implementation challenging
+
     client: Client
 
     stream = "link_send"
     tap_stream_id = "link_send"
     object_ref = "LinkSend"
     key_properties = ["ID"]
-    replication_key = "ModifiedDate"
-    valid_replication_keys = ["ModifiedDate"]
+    # replication_key = "ModifiedDate"
+    # valid_replication_keys = ["ModifiedDate"]
