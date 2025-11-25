abe2282413389976f0c4cecb753973c8bbb4a79e
vishalpachpinde <vpachpinde@talend.com>
2025-11-25 16:28:55 +0530

    fix issue

--- tap_exacttarget/streams/automation.py
+++ tap_exacttarget/streams/automation.py
@@ -40,7 +40,7 @@ class Automation(FullTableStream):
         self, state: Dict, schema: Dict, stream_metadata: Dict, transformer: Transformer
     ) -> Dict:
         """Abstract implementation for `type: Fulltable` stream."""
-        records_processed, = 0
+        records_processed = 0
         for record in self.get_records(stream_metadata, schema):
             transformed_record = transformer.transform(record, schema, stream_metadata)
             write_record(self.tap_stream_id, transformed_record)
